8450eaac048fb36a6be6c90dd0c515132956d199
Mark Potter <si3rr4wow@gmail.com>
2022-11-18 12:28:18 +0000

    syntax

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('I am alive!')
+import prompt, { Schema } from 'prompt'
+
+const schema: Schema = {
+  properties: {
+    documentSyntax: {
+      default: 'json',
+      description: 'Choose a document syntax, yaml or json',
+      message: 'The document syntax must be either yaml or json',
+      pattern: /^yaml|json$/i,
+      required: true,
+      type: 'string',
+      before: (input) => input.toLowerCase(),
+    },
+  },
+}
+
+const { documentSyntax } = await prompt.get(schema)
+console.log(documentSyntax)
