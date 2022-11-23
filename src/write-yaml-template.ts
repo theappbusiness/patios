9d56f707d7060db17d9f1ca9d9478d06b3771fd5
Mark Potter <si3rr4wow@gmail.com>
2022-11-23 17:30:39 +0000

    write json and yaml to bin postbuild

---
+++ src/write-yaml-template.ts
@@ -0,0 +1,45 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { stringify } from 'json-to-pretty-yaml'
+
+const walk = (dir: string, prevResults?: string[]): string[] => {
+  const list = fs.readdirSync(dir)
+  const results = prevResults || []
+  list.forEach((file) => {
+    file = path.resolve(dir, file)
+    const stat = fs.statSync(file)
+    if (stat && stat.isDirectory()) {
+      walk(file, results)
+    } else {
+      results.push(file)
+    }
+  })
+  return results
+}
+
+;((): void => {
+  fs.mkdirSync('./bin/templates')
+  const jsonPaths = walk('./templates/json')
+  jsonPaths.forEach((jsonPath) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const json = require(jsonPath)
+    const data = stringify(json)
+    const binJsonTemplatesPath = jsonPath.replace(
+      '/templates/',
+      '/bin/templates/',
+    )
+    const binYamlTemplatesPath = binJsonTemplatesPath.replaceAll('json', 'yaml')
+    const jsonDirname = path.dirname(binJsonTemplatesPath)
+    const yamlDirname = path.dirname(binYamlTemplatesPath)
+    const jsonStat = fs.statSync(jsonDirname, { throwIfNoEntry: false })
+    const yamlStat = fs.statSync(jsonDirname, { throwIfNoEntry: false })
+    if (!jsonStat?.isDirectory()) {
+      fs.mkdirSync(jsonDirname)
+    }
+    if (!yamlStat?.isDirectory()) {
+      fs.mkdirSync(yamlDirname)
+    }
+    fs.writeFileSync(binJsonTemplatesPath, data)
+    fs.writeFileSync(binYamlTemplatesPath, data)
+  })
+})()
