07c8630b72c5c3405999d71789846528dd13c8e9
Mark Potter <si3rr4wow@gmail.com>
2022-11-24 11:15:22 +0000

    tighter project name regex

--- src/wizard.ts
+++ src/wizard.ts
@@ -16,7 +16,7 @@ export const getProjectNamePrompt = (
   message: 'Project name',
   initial: processDirectory,
   validate: (input) =>
-    !!input.match(/^[\w\-_]+$/) ||
+    !!input.match(/^[a-z0-9\-_]+$/i) ||
     'Project name may only contain letters, numbers, hyphens and underscores',
 })
 
