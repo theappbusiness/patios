2ee5d962aecb020138bec9e1f5ff9754fe32cfd1
Rob Morgan <rob.morgan@theappbusiness.com>
2022-11-25 10:55:09 +0000

    Ensure eslint only applies TypeScript rules to .ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,86 @@
+const jsExtends = [
+  'eslint:recommended',
+  'plugin:import/recommended',
+  'prettier',
+]
+
+const tsExtends = [
+  'eslint:recommended',
+  'plugin:@typescript-eslint/recommended',
+  'plugin:import/recommended',
+  'plugin:import/typescript',
+  'prettier',
+]
+
+const jsPlugins = ['prettier', 'import', 'jest-formatting']
+
+const tsPlugins = ['@typescript-eslint', ...jsPlugins]
+
+const jsParserOptions = {
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+}
+
+const tsParserOptions = {
+  ...jsParserOptions,
+  project: './tsconfig.json',
+}
+
+const jsRules = {
+  eqeqeq: 'error',
+  'import/no-default-export': 'error',
+  'import/no-unresolved': 'error',
+  'import/order': [
+    'error',
+    {
+      pathGroups: [
+        {
+          group: 'external',
+          pattern: '~shared/**',
+          position: 'after',
+        },
+      ],
+      pathGroupsExcludedImportTypes: ['builtin'],
+    },
+  ],
+  'prettier/prettier': [
+    'warn',
+    {
+      endOfLine: 'auto',
+    },
+  ],
+  'jest-formatting/padding-around-describe-blocks': 2,
+  'jest-formatting/padding-around-test-blocks': 2,
+}
+
+const tsRules = {
+  ...jsRules,
+  '@typescript-eslint/explicit-module-boundary-types': 'error',
+  '@typescript-eslint/explicit-function-return-type': 'error',
+  '@typescript-eslint/no-unused-vars': [
+    'error',
+    { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+  ],
+}
+
 module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'prettier',
-  ],
+  extends: jsExtends,
   ignorePatterns: ['**/node_modules'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'prettier', 'import', 'jest-formatting'],
+  parserOptions: jsParserOptions,
+  plugins: jsPlugins,
   root: true,
-  rules: {
-    eqeqeq: 'error',
-    'import/no-default-export': 'error',
-    'import/no-unresolved': 'error',
-    'import/order': [
-      'error',
-      {
-        pathGroups: [
-          {
-            group: 'external',
-            pattern: '~shared/**',
-            position: 'after',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['builtin'],
-      },
-    ],
-    'prettier/prettier': [
-      'warn',
-      {
-        endOfLine: 'auto',
-      },
-    ],
-    'jest-formatting/padding-around-describe-blocks': 2,
-    'jest-formatting/padding-around-test-blocks': 2,
-    '@typescript-eslint/explicit-module-boundary-types': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-    ],
-  },
+  rules: jsRules,
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: tsExtends,
+      parser: '@typescript-eslint/parser',
+      parserOptions: tsParserOptions,
+      plugins: tsPlugins,
+      rules: tsRules,
+    },
+  ],
 }
