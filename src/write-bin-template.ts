4e9072d88b33b77f156dec6f2072ca2f118267ec
Mark Potter <si3rr4wow@gmail.com>
2022-11-28 17:15:20 +0000

    ditch post build, link yaml correctly

--- src/write-bin-template.ts
+++ src/write-bin-template.ts
@@ -17,7 +17,7 @@ const walk = (dir: string, prevResults?: string[]): string[] => {
   return results
 }
 
-;((): void => {
+const makeBinTemplateDirectory = (): void => {
   const binTemplatesStat = fs.statSync('./bin/templates', {
     throwIfNoEntry: false,
   })
@@ -25,25 +25,56 @@ const walk = (dir: string, prevResults?: string[]): string[] => {
     fs.rmSync('./bin/templates', { recursive: true })
   }
   fs.mkdirSync('./bin/templates')
-  const jsonPaths = walk('./templates/json')
-  jsonPaths.forEach((jsonPath) => {
-    const json = fs.readFileSync(jsonPath).toString()
-    const binJsonTemplatesPath = jsonPath.replace(
-      '/templates/',
-      '/bin/templates/',
-    )
-    const binYamlTemplatesPath = binJsonTemplatesPath.replaceAll('json', 'yaml')
-    const jsonDirname = path.dirname(binJsonTemplatesPath)
-    const yamlDirname = path.dirname(binYamlTemplatesPath)
-    const jsonStat = fs.statSync(jsonDirname, { throwIfNoEntry: false })
-    const yamlStat = fs.statSync(yamlDirname, { throwIfNoEntry: false })
-    if (!jsonStat?.isDirectory()) {
-      fs.mkdirSync(jsonDirname)
-    }
-    if (!yamlStat?.isDirectory()) {
-      fs.mkdirSync(yamlDirname)
-    }
-    fs.writeFileSync(binJsonTemplatesPath, json)
-    fs.writeFileSync(binYamlTemplatesPath, stringify(JSON.parse(json)))
+}
+
+const getBinTargetPaths = (
+  jsonPath: string,
+): { jsonBinPath: string; yamlBinPath: string } => {
+  const jsonBinPath = jsonPath.replace('/templates/', '/bin/templates/')
+  const yamlBinPath = jsonBinPath.replaceAll('json', 'yaml')
+  return {
+    jsonBinPath,
+    yamlBinPath,
+  }
+}
+
+const makeBinTemplateSubdirectory = (jsonPath: string): void => {
+  const { jsonBinPath, yamlBinPath } = getBinTargetPaths(jsonPath)
+  const jsonDirname = path.dirname(jsonBinPath)
+  const yamlDirname = path.dirname(yamlBinPath)
+  const jsonStat = fs.statSync(jsonDirname, { throwIfNoEntry: false })
+  const yamlStat = fs.statSync(yamlDirname, { throwIfNoEntry: false })
+  if (!jsonStat?.isDirectory()) {
+    fs.mkdirSync(jsonDirname)
+  }
+  if (!yamlStat?.isDirectory()) {
+    fs.mkdirSync(yamlDirname)
+  }
+}
+
+const getTemplateStrings = (
+  jsonPath: string,
+): { jsonStr: string; yamlStr: string } => {
+  const rawTemplate = fs.readFileSync(jsonPath).toString()
+  const jsonStr = rawTemplate.replaceAll('$EXT', 'json')
+  const yamlStr = stringify(JSON.parse(rawTemplate.replaceAll('$EXT', 'yaml')))
+  return {
+    jsonStr,
+    yamlStr,
+  }
+}
+
+const writeTemplateFileToBin = (jsonPath: string): void => {
+  makeBinTemplateSubdirectory(jsonPath)
+  const { jsonStr, yamlStr } = getTemplateStrings(jsonPath)
+  const { jsonBinPath, yamlBinPath } = getBinTargetPaths(jsonPath)
+  fs.writeFileSync(jsonBinPath, jsonStr)
+  fs.writeFileSync(yamlBinPath, yamlStr)
+}
+
+;((): void => {
+  makeBinTemplateDirectory()
+  walk('./templates/json').forEach((jsonPath) => {
+    writeTemplateFileToBin(jsonPath)
   })
 })()
